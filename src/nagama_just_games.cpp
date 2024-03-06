3e42acae5403a4da83e5f385b5d039f2d0aacab6
Alvin Wong <alvinwong@ufl.edu>
2024-03-06 17:55:21 -0500

    Removed repeats

--- src/nagama_just_games.cpp
+++ src/nagama_just_games.cpp
@@ -149,12 +149,8 @@ void displayDinosaur() {
     if(dinosaurSpeed >= 0) { //going right
       display.fillTriangle(dinosaurX+SCREEN_WIDTH/2-15, SCREEN_HEIGHT-5, dinosaurX+SCREEN_WIDTH/2-3, SCREEN_HEIGHT-5, dinosaurX+SCREEN_WIDTH/2, SCREEN_HEIGHT-22, SH110X_WHITE);
       display.fillTriangle(dinosaurX+SCREEN_WIDTH/2-15+floor(float(NUTRITION)/100.0*14.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2-4+floor(float(NUTRITION)/100.0*3.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2-1, SCREEN_HEIGHT-22, SH110X_BLACK);
-      display.fillTriangle(dinosaurX+SCREEN_WIDTH/2-15+floor(float(NUTRITION)/100.0*14.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2-4+floor(float(NUTRITION)/100.0*3.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2-1, SCREEN_HEIGHT-22, SH110X_BLACK);
     } else { //going left
       display.fillTriangle(dinosaurX+SCREEN_WIDTH/2+15, SCREEN_HEIGHT-5, dinosaurX+SCREEN_WIDTH/2+3, SCREEN_HEIGHT-5, dinosaurX+SCREEN_WIDTH/2, SCREEN_HEIGHT-22, SH110X_WHITE);
-      //dinosaurX+SCREEN_WIDTH/2, SCREEN_HEIGHT-22
-      display.fillTriangle(dinosaurX+SCREEN_WIDTH/2+15-floor(float(NUTRITION)/100.0*15.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2+3-floor(float(NUTRITION)/100.0*3.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2, SCREEN_HEIGHT-22, SH110X_BLACK);
-      //dinosaurX+SCREEN_WIDTH/2, SCREEN_HEIGHT-22
       display.fillTriangle(dinosaurX+SCREEN_WIDTH/2+15-floor(float(NUTRITION)/100.0*15.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2+3-floor(float(NUTRITION)/100.0*3.0), SCREEN_HEIGHT-5-floor(float(NUTRITION)/100.0*17.0), dinosaurX+SCREEN_WIDTH/2, SCREEN_HEIGHT-22, SH110X_BLACK);
     }
 
